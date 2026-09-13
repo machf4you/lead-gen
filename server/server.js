6b0cdd7ae70d6179cde656acc7101da6625f687e
Mac McCarthy <macone157@gmail.com>
2026-09-13 13:06:19 +0200

    Fix import util in server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { exec } from 'child_process';
+import util from 'util';
 import nodemailer from 'nodemailer';
 import { getDb, getSenderSettings, updateSenderSettings } from './db.js';
 
